83ac1887687fbdcb202bb4f4d418e48ec0d30e90
k-roozbahani <kavehrozbah@gmail.com>
2022-10-19 12:46:49 +0330

    make model Expert

--- experts/models.py
+++ experts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Expert(models.Model):
+    SITE_CHITGHAR = 1
+    SITE_ZARAFSHAN = 2
+    SITE_SEMNAN = 3
+    SITES = ((SITE_SEMNAN, 'site Chitghar'), (SITE_ZARAFSHAN, 'site Zarafshan'), (SITE_SEMNAN, 'site Semnan'))
+    user = models.OneToOneField('User', models.CASCADE)
+    personal_id = models.IntegerField(unique=True)
+    date_employment = models.DateField("date employment", null=True, blank=True)
+    site = models.SmallIntegerField('site', default=1, choices=SITES)
+
